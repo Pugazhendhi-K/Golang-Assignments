63126c2d2d3d053412c6029633cc449902a2f3f2
Pugazhendhi_K <pugazhkv@gmail.com>
2020-10-22 16:48:22 +0530

    Create Functioneg.go

---
+++ Functioneg.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func login(name string) string {
+	return "Hello, " + name
+	
+}
+
+func main() {
+        var n string = "Pugazh"
+	fmt.Println(login("Welcome"))
+	fmt.Println(n)
+	}g
