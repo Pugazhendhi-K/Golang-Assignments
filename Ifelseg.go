99f5074fa469983270268a7c0dabeedf7b970050
Pugazhendhi_K <pugazhkv@gmail.com>
2020-10-23 20:54:33 +0530

    Create Ifelseg.go

---
+++ Ifelseg.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    
+    num := -25
+    
+    if num > 0 {
+
+        fmt.Println("The number is positive")
+    } else {
+
+        fmt.Println("The number is negative")
+    }
+}
