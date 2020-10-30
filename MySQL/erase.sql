da61dfcc3837e19e9fda486eb41cc58c6c09c640
Pugazhendhi_K <pugazhkv@gmail.com>
2020-10-30 23:32:00 +0530

    Update erase.sql

--- MySQL/erase.sql
+++ MySQL/erase.sql
@@ -1,6 +1,16 @@
 CREATE TABLE user (
     userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100),
-    password VARCHAR(100) 
-)
+    password VARCHAR(100) )
 
+insert into user (userId, username, password) values (1, 'Arun', '45q');
+insert into user (userId, username, password) values (2, 'Ramesh', '580ig7800');
+insert into user (userId, username, password) values (3, 'John', '68gjjj');
+
+//Deletes the Records of the Table only
+
+TRUNCATE TABLE user;
+
+//Deletes the Entire Table
+
+DROP TABLE user;
