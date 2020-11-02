cd7ef51f87c804aa30514aa88dc6ffd4a75eb197
Pugazhendhi_K <pugazhkv@gmail.com>
2020-11-02 18:45:16 +0530

    Create main.go

---
+++ Microservices/JSON-validation/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/Pugazhendhi-K/Kloudone_Training/tree/main/Microservices/JSON-validation/handlers"
+	"github.com/Pugazhendhi-K/Kloudone_Training/tree/main/Microservices/env"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
+func main() {
+
+	env.Parse()
+
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+
+	ph := handlers.NewProducts(l)
+
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+}
